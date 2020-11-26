41f915510406c264f6fdf49a70392820a8eff607
Alexandr Travnikov <alextr663@gmail.com>
2020-11-26 17:14:35 +0300

    Текст и checkbox

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let obj = {
-    "type": "one", // text, one, many
+    "type": "text", // text, one, many
     "question": "Сколько будет 5 * 23",
-    //"answer": "115",
+    "answer": "115",
     "answers": [{
             "answer": "100",
             "correct": false
@@ -24,32 +24,51 @@ let obj = {
 let question = document.querySelector("h1");
 question.innerText = obj.question;
 
-for (let i = 0; i < obj.answers.length; i++) {
-    let answer = document.createElement("div");
-    let input = document.createElement("input");
-    input.type = "radio";
-    input.name = `question`;
-    input.id = obj.answers[i].correct;
-    answer.appendChild(input);
-    let label = document.createElement("label");
-    label.innerText = obj.answers[i].answer;
-    answer.appendChild(label);
-    document.body.appendChild(answer);
-}
 let button = document.createElement("button");
 button.innerText = "Отправить";
-button.onclick = function() {
-    let counter = 0;
-    let inputs = document.querySelectorAll("input");
-    for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked === (inputs[i].id === "true")) {
-            counter++;
+
+if (obj.type === "text") {
+    let answer = document.createElement("div");
+    let input = document.createElement("input");
+    input.type = "text";
+    answer.appendChild(input);
+    document.body.appendChild(answer);
+    button.onclick = function() {
+        if (document.querySelector("input").value === obj.answer) {
+            alert('Правильно!')
+        } else {
+            alert('Неправильно!')
         }
     }
-    if (counter === inputs.length) {
-        alert('Правильно!')
-    } else {
-        alert('Неправильно!')
+} else {
+    for (let i = 0; i < obj.answers.length; i++) {
+        let answer = document.createElement("div");
+        let input = document.createElement("input");
+        input.type = "radio";
+        if (obj.type === "many") {
+            input.type = "checkbox";
+        }
+        input.name = `question`;
+        input.id = obj.answers[i].correct;
+        answer.appendChild(input);
+        let label = document.createElement("label");
+        label.innerText = obj.answers[i].answer;
+        answer.appendChild(label);
+        document.body.appendChild(answer);
+    }
+    button.onclick = function() {
+        let counter = 0;
+        let inputs = document.querySelectorAll("input");
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked === (inputs[i].id === "true")) {
+                counter++;
+            }
+        }
+        if (counter === inputs.length) {
+            alert('Правильно!')
+        } else {
+            alert('Неправильно!')
+        }
     }
 }
 document.body.appendChild(button);
